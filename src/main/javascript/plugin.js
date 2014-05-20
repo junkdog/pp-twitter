ae5a759c7884131aa35babb31e2c4b867510bfa7
Adrian Papari <blackrax@gmail.com>
2014-05-20 10:38:59 +0200

    initial commit

---
+++ src/main/javascript/plugin.js
@@ -0,0 +1,8 @@
+/*!
+ * Dummy javascript resource that serves as an example for resource compression
+ * com.atex.plugins:twitter
+ *
+ * This license style comment should be preserved after compression
+ */
+// This comment should be removed though
+//
